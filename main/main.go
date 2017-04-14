46997db833198e79a7442592c2ed18fffc3f7681
mb <mbritton@andrew.cmu.edu>
2017-04-14 19:06:15 -0400

    changed println to fmt.Println

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	zmq4 "github.com/pebbe/zmq4"
 	"strconv"
+	"fmt"
 )
 func main(){
 	if len(os.Args) < 3{
@@ -15,12 +16,12 @@ func main(){
 	var configs Configs = ReadConfig(configfile)
 	self := getNodeInfo(selfstr, configs)
 	leader := getNodeInfo("leader", configs)
-	println("Running as " + self.NodeName)
-	println("IP: " + self.NodeAddr)
-	println("Port1: " + self.SendPort)
-	println("Port2: " + self.RecvPort)
-	println("Group: " + self.NodeGroup)
-	println("Effort Level: " + self.Effort)
+	fmt.Println("Running as "+self.NodeName)
+	fmt.Println("IP: " + self.NodeAddr)
+	fmt.Println("Port1: " + self.SendPort)
+	fmt.Println("Port2: " + self.RecvPort)
+	fmt.Println("Group: " + self.NodeGroup)
+	fmt.Println("Effort Level: " + self.Effort)
 	cntxt,_ := zmq4.NewContext()
 	eff, _ := strconv.Atoi(self.Effort)
 	setEffort(eff)

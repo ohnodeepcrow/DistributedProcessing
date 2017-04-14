79f15f9218bed77b366611c044763848e457717d
mb <mbritton@andrew.cmu.edu>
2017-04-14 17:24:25 -0400

    Added message.go

--- main/utils.go
+++ main/utils.go
@@ -5,3 +5,19 @@ import "time"
 func getCurrentTimestamp() string{
 	return time.Now().Format("15:04:05")
 }
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+
+func getNodeInfo(self string, config Configs) NodeInfo{
+	for i := 0; i < len(config.Nodes); i++ {
+		if config.Nodes[i].NodeName == self{
+			return config.Nodes[i]
+		}
+	}
+	panic("Node doesn't exist!")
+}
